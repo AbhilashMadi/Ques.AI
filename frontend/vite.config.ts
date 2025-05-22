8d810d9e87922e241e58cfdd61ad95609e89c8bd
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-05-22 16:42:48 +0530

    feat(frontend): theme CSS variables, theme context

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsConfigPathsPlugin from 'vite-tsconfig-paths'
+import tailwindVitePlugin from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsConfigPathsPlugin(),
+    tailwindVitePlugin()
+  ],
+  server: {
+    open: true,
+    port: 3000,
+  },
 })

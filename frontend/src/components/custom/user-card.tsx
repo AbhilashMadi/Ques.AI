8ee710f646ffb7a48d565b8b1e303a2fc2debc38
Abhilash Kumar <madiabhilash1563@gmail.com>
2025-05-29 00:20:33 +0530

    fix: user card detilas

--- frontend/src/components/custom/user-card.tsx
+++ frontend/src/components/custom/user-card.tsx
@@ -1,3 +1,4 @@
+import useAuth from '@hooks/use-auth';
 import { SitePaths } from '@configs/site-config';
 import '@css/avatar-sprit.css';
 import { type FC } from 'react';
@@ -8,12 +9,14 @@ interface IUserCardProps {
 }
 
 const UserCard: FC<IUserCardProps> = ({ collapse }) => {
+  const { user } = useAuth();
+
   return (<Link to={SitePaths.USER_PROFILE} className="p-2 rounded">
     <section className="flex gap-2">
       <div className="avatar-sm avatar-1 rounded" />
       {!collapse && <div>
-        <p>Username</p>
-        <p className="text-muted">user@email.com</p>
+        <p>{user?.fullName}</p>
+        <p className="text-muted">{user?.email}</p>
       </div>}
     </section>
   </Link>);
